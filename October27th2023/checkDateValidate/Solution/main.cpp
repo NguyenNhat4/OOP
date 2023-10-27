70687de0929c88ee7bd649fae8f90e2ffbf5dbd7
Nguyễn minh nhật <123147485+NguyenNhat4@users.noreply.github.com>
2023-10-27 21:27:52 +0700

    delete abundant comment

--- October27th2023/checkDateValidate/Solution/main.cpp
+++ October27th2023/checkDateValidate/Solution/main.cpp
@@ -1,11 +1,4 @@
 
-/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
-define
-include
-using
-struct
-###End banned keyword*/
-
 #include <iostream>
 #include <math.h>
 
